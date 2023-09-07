6345a047f0b1eecbdabbffe3a83f39f87d69fd0d
JShoon <jsh021813@gmail.com>
2023-09-07 22:13:34 +0900

    23.09.07 강의 예제

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
 
 export class ExpenseList extends Component {
   render() {
+    console.log(this.props.initialExpenses);
     return (
-      <div>ExpenseList</div>
-    )
+      <>
+        <ul className="list">
+          {/* Expense Item */}
+          {this.props.initialExpenses.map((expense) => {
+            return (
+               <ExpenseItem 
+                  expense={expense}
+                  key={expense.id}
+                  handleDelete = {this.props.handleDelete}
+                />
+              )
+          })}
+        </ul>
+        <button className="btn">
+          목록 지우기
+          <MdDelete className="btn-icon" />
+        </button>
+      </>
+    );
   }
 }
 
-export default ExpenseList
+export default ExpenseList;
